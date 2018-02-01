d535165271312e3d4a4fbac7e35409b2d11bab35
Kory <koryskarbek@yahoo.com>
2018-02-01 14:14:45 -0800

    server file and server connection

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -4,81 +4,111 @@ var myApp = new Framework7();
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
-});
+$.support.cors = true;
+
+// variables for server url
+var url = 'http://staging.redleafhq.com/';
+var file = 'semicdraw.php';
+var nonRepresentational = '?s=non-representational';
+var representational = '?s=representational';
+var imaginative = '?s=imaginative';
+var random = '?s=rando';
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
-  console.log("Device is ready!");
-  $$('body').css('background-image', localStorage.getItem("background"));
-  $$('#info-div').css('background-image', localStorage.getItem("background"));
+  console.log('Device is ready!');
+  $$('body').css('background-image', localStorage.getItem('background'));
+  $$('#info-div').css('background-image', localStorage.getItem('background'));
+  if(localStorage.getItem('category-image') != null) {
+    $$('#prompt-category').attr('src', localStorage.getItem('category-image'));
+  }
+  if(localStorage.getItem('current-keyword') != null) {
+    $$('#keyword').text(localStorage.getItem('current-keyword'));
+  }
 });
 
-// document.addEventListener('deviceready', onDeviceReady, false);
+// function to get prompt from server
+function getKeyword(table) {
+  var request = new XMLHttpRequest();
+  var serverUrl = url + file + table;
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       var prompt = JSON.parse(this.responseText)
+       document.getElementById('keyword').innerHTML = prompt.keywords;
+       localStorage.setItem('current-keyword', prompt.keywords);
+    }
+  };
+  request.open('POST', serverUrl, true);
+  request.send();
+}
 
-// function onDeviceReady() {
-//   var db = window.sqlitePlugin.openDatabase({name: 'prompts.db', location: 'default'});
-//   db.executeSql("SELECT * FROM imaginative");
-//   window.sqlitePlugin.echoTest(function() {
-//     console.log('ECHO test OK');
-//   });}
-
-// function randomIndex() {
-//   var max = 0;
-//   db.readTransaction(function(tx) {
-//     max += tx.executeSql("SELECT COUNT(index) FROM imaginative")
-//   });
-//     return Math.floor(Math.random() * max) + 1;
-// }
-
-// $$('#menu-closed').on('click', function() {
-//   $$('#menu-closed').hide();
-//   $$('#menu-icons').show();
-// });
-
+// click to see prompt category folders
 $$('#prompt-category').on('click', function() {
   $$('#prompt-folders').show();
   $$('#color-options').hide();
 });
 
+// click on specific prompt category folder
 $$('.prompt-buttons').on('click', function() {
   var imageUpdate = $$(this).attr('src');
-  // localStorage.setItem('prompt', '');
+  if($$(this).attr('id') == 'all-categories') {
+    getKeyword(random);
+    localStorage.setItem('current-category', 'all');
+  }
+  if($$(this).attr('id') == 'non-rep-categories') {
+    getKeyword(nonRepresentational);
+    localStorage.setItem('current-category', 'non-representational');
+  }
+  if($$(this).attr('id') == 'imaginative-categories') {
+    getKeyword(imaginative);
+    localStorage.setItem('current-category', 'imaginative');
+  }
+  if($$(this).attr('id') == 'rep-categories') {
+    getKeyword(representational);
+    localStorage.setItem('current-category', 'representational');
+  }
+  localStorage.setItem('category-image', imageUpdate);
   $$('#prompt-category').attr('src', imageUpdate);
   $$('#prompt-folders').hide();
 });
 
-// $$('#imaginative-categories').on('click', function() {
-//   function getImaginativePrompt() {
-//     db.readTransaction(function(tx) {
-//       tx.executeSql("SELECT prompt FROM imaginative WHERE id=1")
-//       document.getElementById('prompt').innerHTML = result.row;
-//     });
-//   }
-// });
-
+// click to show background color options
 $$('#background-color').on('click', function() {
   $$('#color-options').show();
   $$('#prompt-folders').hide();
 });
 
+// click to choose background color
 $$('.color-buttons').on('click', function() {
-  var backgroundColor = "../www/img/" + $$(this).attr('id') + ".png";
+  var backgroundColor = '../www/img/' + $$(this).attr('id') + '.png';
   localStorage.setItem('background', 'url(' + backgroundColor + ')');
   $$('body').css('background-image', 'url(' + backgroundColor + ')');
   $$('#info-div').css('background-image', 'url(' + backgroundColor + ')');
   $$('#color-options').hide();
 });
 
+// click area is all but bottom option bar. Clicking here will give new drawing prompt
 $$('#click-area').on('click', function() {
+  if(localStorage.getItem('current-keyword') != null) {
+    if(localStorage.getItem('current-category') == 'all') {
+      getKeyword(random);
+    }
+    if(localStorage.getItem('current-category') == 'non-representational') {
+      getKeyword(nonRepresentational);
+    }
+    if(localStorage.getItem('current-category') == 'representational') {
+      getKeyword(representational);
+    }
+    if(localStorage.getItem('current-category') == 'imaginative') {
+      getKeyword(imaginative);
+    }
+  }
   $$('#color-options').hide();
   $$('#prompt-folders').hide();
   $$('#info-div').hide();
 })
 
+// click to show info div
 $$('#info-button').on('click', function() {
   $$('#info-div').show();
 })
